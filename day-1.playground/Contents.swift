35cc80ac8d19892629f688ce20b337e1c27e8b98
diosnelv <>
2020-01-14 13:38:36 -0300

    day 1

---
+++ day-1.playground/Contents.swift
@@ -0,0 +1,38 @@
+import UIKit
+
+var str = "Hello, playground"
+
+str = "God Bye!!"
+
+var age = 38
+
+var population = 8_000_000
+
+var str1 = """
+This goes
+over multiple
+lines
+"""
+
+var str2 = """
+This goes \
+over multiple \
+lines
+"""
+
+var pi = 3.141
+
+var awesome = true
+
+var score = 85
+var str3 = "Your score was \(score * 3)"
+var results = "The test results are here: \(str3)"
+
+let taylor = "swift"
+
+let str4 = "Hello, playground"
+
+let album: String = "Reputation"
+let year: Int = 1989
+let height: Double = 1.78
+let taylorRocks: Bool = true
